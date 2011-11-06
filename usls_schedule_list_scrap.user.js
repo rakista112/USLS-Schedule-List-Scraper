9021ac1e2d18b1b8a327e18dd46a4e71db55a31f
Ray John <rakista112@gmail.com>
2011-11-06 17:09:50 +0800

    Removed nonsense metas.
    Signed-off-by: Ray John <rakista112@gmail.com>

--- usls_schedule_list_scrap.user.js
+++ usls_schedule_list_scrap.user.js
@@ -1,12 +1,9 @@
 // ==UserScript==
 // @name USLS Schedule List Scraper
 // @description Makes it easier to extract and save your schedule in other file formats
-// @include http://localhost/gmScripts/USLS_Sched_Scraper*
 // @include http://*students.usls.edu.ph/index.cfm?*
 // @version 0.42
 // @copyright Ray John E. Alovera
-// @run-at document-end
-// @unwrap
 // ==/UserScript==
 
 // ui-tabs-1
